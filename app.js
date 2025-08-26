a4b550653cbbe6634e0682c79bb292cd0c8204ea
Carlos Valladolid <lachoman@hotmail.com>
2025-08-26 16:53:57 -0600

    Actualizar server 3 26-Agosto-2025

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 const { EVENTS } = require('@bot-whatsapp/bot')
 
-const serverNumber = "8124247504";
+const serverNumber = "8124267007";
 
 // Function to call a web API
 async function callWebApi(url, message) {
@@ -31,7 +31,7 @@ async function callWebApi(url, message) {
 const flowPrincipal = addKeyword(['Estatus'])
     .addAnswer('Corriendo...')
 
-const flowStatus = addKeyword(['Hola 8124247504'])
+const flowStatus = addKeyword(['Hola 8124267007'])
     .addAnswer('SI-' + serverNumber)
 
 //const flowBienvenida = addKeyword(EVENTS.WELCOME)
